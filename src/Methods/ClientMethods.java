baa953a1603b90c24b7fe6df5383963f4523f8ec
JoelPitaVidal <145323213+JoelPitaVidal@users.noreply.github.com>
2025-02-03 17:51:56 +0100

    Cambios añadidos manualmente a causa de un fallo

--- src/Methods/ClientMethods.java
+++ src/Methods/ClientMethods.java
@@ -1,6 +1,29 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class ClientMethods {
 
+    public void showResult(String [] result){
+        System.out.println("The result is: " + result[3]);
+    }
 
+
+        public void makeOperation(String[] operacion){
+
+        System.out.println("Enter the first number of the operation: ");
+        Scanner sc = new Scanner(System.in);
+        String number1 = sc.nextLine();
+
+        System.out.println("Enter an operation:");
+        String operator = sc.nextLine();
+
+        System.out.println("Enter the second number of the operation:");
+        String number2 = sc.nextLine();
+    
+        operacion[0] = number1;
+        operacion[1] = operator;
+        operacion[2] = number2;
+
+    }
 }
